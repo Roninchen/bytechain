86c317f23e3d38fcc8b1d77dd9338ee48ba6e6fe
易达 <yida@yidadeMBP.lan>
2019-03-31 20:50:49 +0800

    bolt db

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
 
 	//new block
 
-	blockchian.AddBlockToBlockChain("send 100 RMB to TOM", blockchian.Blocks[len(blockchian.Blocks)-1].Height+1, blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+	blockchian.AddBlockToBlockChain("send 100 RMB to TOM")
 
-	blockchian.AddBlockToBlockChain("send 100 RMB to Bob", blockchian.Blocks[len(blockchian.Blocks)-1].Height+1, blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+	blockchian.AddBlockToBlockChain("send 100 RMB to Bob")
 
-	blockchian.AddBlockToBlockChain("send 100 RMB to Alia", blockchian.Blocks[len(blockchian.Blocks)-1].Height+1, blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+	blockchian.AddBlockToBlockChain("send 100 RMB to Alia")
 
-	blockchian.AddBlockToBlockChain("send 100 RMB to Ada", blockchian.Blocks[len(blockchian.Blocks)-1].Height+1, blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+	blockchian.AddBlockToBlockChain("send 100 RMB to Ada")
 }

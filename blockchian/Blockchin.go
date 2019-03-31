e477f867cea73a504768e6be7ae032380785fdca
易达 <yida@yidadeMBP.lan>
2019-03-31 12:10:36 +0800

    add block

--- blockchian/Blockchin.go
+++ blockchian/Blockchin.go
@@ -1,1 +1,17 @@
 package blockchian
+
+type BlockChain struct {
+	Blocks []*Block // stored sort block
+}
+
+// 1. create block with genesis block
+func CreateBlockChainWithGenesisBlock() *BlockChain {
+	genesisBlock := CreateGenesisBlock("genesis block .....")
+	return &BlockChain{[]*Block{genesisBlock}}
+}
+
+// 2 add block
+func (blc *BlockChain) AddBlockToBlockChain(data string,height int64,preHash []byte) {
+	newBlock := NewBlock(data, height, preHash)
+	blc.Blocks = append(blc.Blocks, newBlock)
+}
--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 )
 
 func main() {
-	genesisBlock := blockchian.CreateGenesisBlock("创世区块")
-	fmt.Println(genesisBlock)
+	blockchian := blockchian.CreateBlockChainWithGenesisBlock()
+
+	fmt.Println(blockchian.Blocks[0])
+
+	//new block
+
+	blockchian.AddBlockToBlockChain("send 100 RMB to TOM",blockchian.Blocks[len(blockchian.Blocks)-1].Height+1,blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+
+	blockchian.AddBlockToBlockChain("send 100 RMB to Bob",blockchian.Blocks[len(blockchian.Blocks)-1].Height+1,blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+
+	blockchian.AddBlockToBlockChain("send 100 RMB to Alia",blockchian.Blocks[len(blockchian.Blocks)-1].Height+1,blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+
+	blockchian.AddBlockToBlockChain("send 100 RMB to Ada",blockchian.Blocks[len(blockchian.Blocks)-1].Height+1,blockchian.Blocks[len(blockchian.Blocks)-1].Hash)
+
+
+	fmt.Println(blockchian.Blocks)
+
 }
